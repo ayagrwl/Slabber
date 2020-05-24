b75f981ddd0393407d80f45386958c2c6f9d9910
Vinesh <Vinesh.katewa@gmail.com>
2020-05-24 13:01:55 +0530

    Created chatrooms and chat functionality's skeleton

--- routes/chatroom.js
+++ routes/chatroom.js
@@ -4,23 +4,27 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/admin/:adminID', (req, res, next) => {
-    var adminId = req.params.adminID;
-    res.render('../template/admin.ejs', {admin: adminId});
+app.get('/chatroom/:roomid', (req, res, next) => {
+    var roomid = req.params.roomid;
+    res.render('../template/chatroom.ejs', {room: roomid, roomname: 'Chat Room '+roomid});
+});
+
+app.get('/chat/:chatid', (req, res, next) => {
+    var chatid = req.params.chatid;
+    res.render('../template/chat.ejs', {chat: chatid, friendName: 'Friend'});
 });
 
 module.exports = {
     socket: function(io) {
-        io.of(/^\/admin\/\d+$/).on('connection', (socket) => {
-            console.log('fuck! admin is here!');
-            socket.emit('hemlo', 'fuck you');
-            socket.on('hemlo', socket => {
-                console.log(socket);
-            });
+        io.of(/^\/chatroom\/\d+$/).on('connection', (socket) => {
             socket.on('msg', (socket) => {
                 console.log(socket);
-            })
+                io.of('/chatroom/'+socket.id).emit('msg', socket.msg);
+            });
         });
+        io.of(/^\/chat\/\d+$/).on('connection', (socket) => {
+            console.log("fuck yeah");
+        })
     },
     app: app
 }
