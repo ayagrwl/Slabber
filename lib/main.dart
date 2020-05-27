0f3aa98cb56dd19a153d21f63c66a1c52a8d7f2e
creepy23 <61056478+creepy23@users.noreply.github.com>
2020-05-28 02:32:52 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui_starter/screens/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Slabber',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.lightBlue,
+        accentColor: Color(0xFFFEF9EB),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
