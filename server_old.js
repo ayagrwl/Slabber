0949ae7dc8a94f00dd399dfcc0d29e71872fa9f4
Vinesh <Vinesh.katewa@gmail.com>
2020-05-28 12:02:34 +0530

    Created server_old.js file

---
+++ server_old.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const http = require('http');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const passport = require('passport');
+const socketIO = require('socket.io');
+const _ = require('lodash');
+
+const users = require('./controllers/users.js')()
+const chats = require('./controllers/chats.js')()
+
+const expressValidator = require('express-validator');
+
+
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/slabberdb', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const app = SetupExpress();
+
+function SetupExpress(){
+    const app = express();
+    const server = http.createServer(app);
+    const io = socketIO(server);
+    require('./socket/groupchat.js')(io);
+
+    server.listen(process.env.PORT || 3000, ()=>{
+        console.log('Listening on port 3000');
+    });
+    ConfigureExpress(app);
+    
+    
+    //Setup router
+    const router = require('express-promise-router')();
+    users.SetRouting(router);
+    chats.SetRouting(router);
+    app.use(router);
+    app.use(function(req, res){
+        res.render('404');
+    });
+    return app;
+}
+
+function ConfigureExpress(app){
+    
+    require('./passport/passport-loc');
+    require('./passport/passport-fb');
+    require('./passport/passport-goog');
+    
+    app.use(express.static('public'));
+    app.set('view engine', 'ejs');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended: true}));
+    
+    app.use(session({
+        secret: 'bhangbhosda',
+        resave: true,
+        saveUninitialized: false,
+        store: new MongoStore({mongooseConnection: mongoose.connection})
+    }));
+    
+    app.use(flash());
+    
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
+    // used in template engine to handle arrays in ejs.
+    app.locals._  = _;
+}
